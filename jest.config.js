a60428dfef7a460812f7b18d5280ba22293b186e
NithinRegidi <regidinithinsainaidu@gmail.com>
2025-08-31 13:32:26 +0530

    jest.config.js and HelloWorld.test.tsx files are added to the project for testing purpose

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  testEnvironment: 'jsdom',
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  testMatch: ['**/src/**/*.test.(js|jsx|ts|tsx)'],
+  moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx', 'json', 'node'],
+};
